0992c8ae923ad2060c03c6e6a37b261aec7ace2b
P V Sanjiv <124117805+sanjivpolamuri@users.noreply.github.com>
2024-07-25 19:18:16 +0530

    Delete passwordgenerator.py

--- passwordgenerator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-lower = "abcdefghijklmnopqrstvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTVWXYZ"
-numbers = "0123456789"
-symbols = "!@#$%^&*()_-+=?>< ["
-all_chars = lower + upper + numbers + symbols length = int(input("Enter a length: "))
-password = ''.join(random.sample (all_chars, length)) print("Generated Password: ", password)
